600f924c31373f899f9ab31afff57acbe8ca5434
Denis Dourado <denis.dourado@um.es>
2026-02-16 23:05:43 +0100

    Añadir gráfica de estadísticas de brazos por algoritmo

--- k_brazos/src/plotting/plotting.py
+++ k_brazos/src/plotting/plotting.py
@@ -17,6 +17,7 @@ from typing import List
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 
 from algorithms import Algorithm, EpsilonGreedy
 
@@ -108,3 +109,63 @@ def plot_regret(steps: int, regret_accumulated: np.ndarray, algorithms: List[Alg
   plt.legend(title='Algoritmos')
   plt.tight_layout()
   plt.show()
+
+
+def plot_arm_statistics(arm_stats: np.ndarray, algorithms: List[Algorithm]):
+    """
+    Genera gráficas separadas de selección de brazos: ganancias vs pérdidas para cada algoritmo.
+    :param arm_stats: Lista de listas de diccionarios con estadísticas de cada brazo por algoritmo.
+    :param algorithms: Lista de instancias de algoritmos comparados.
+    """
+    # Configurar el tema de seaborn
+    sns.set_theme(style="whitegrid", palette="muted", font_scale=1.2)
+
+    for algo_idx, algo_stats in enumerate(arm_stats):
+        # Extraer el nombre del algoritmo
+        algo_name = get_algorithm_label(algorithms[algo_idx])
+
+        # Rango con el número de brazos
+        x = range(len(algo_stats))
+
+        # Extraer los valores del diccionario
+        y_n_selected = [algo_stats[i]["n_times_selected"] for i in x]
+        y_avg_reward = [algo_stats[i]["avg_reward"] for i in x]
+        y_is_optimal = [algo_stats[i]["is_optimal"] for i in x]
+
+        # Array con un 1 en la posición del brazo óptimo
+        optimal_index = y_is_optimal.index(True)
+
+        # Graficar barras
+        plt.figure()
+        bars = plt.bar(x, y_avg_reward, color="blue")
+
+        # Cambiar el color del brazo óptimo
+        bars[optimal_index].set_color("gold")
+
+        # Añadir etiquetas con el número de veces seleccionado de cada brazo
+        for i, bar in enumerate(bars):
+            plt.text(
+                bar.get_x() + bar.get_width() / 2,
+                bar.get_height(),
+                f"z={y_n_selected[i]}",
+                ha="center",
+                va="bottom",
+                fontsize=10
+            )
+
+        # Configurar ejes y título
+        plt.title(f"{algo_name}", fontsize=14)
+        plt.xlabel("Brazo", fontsize=12)
+        plt.ylabel("Recompensa Promedio", fontsize=12)
+        plt.xticks(ticks=x, labels=[str(i) for i in x])
+
+        # Crear una leyenda con información
+        plt.legend(handles=[
+            Patch(facecolor="blue", label="Brazo No Óptimo"),
+            Patch(facecolor="gold", label=f"Brazo Óptimo = {optimal_index}"),
+            Patch(facecolor="none", label="z = Número de Veces Seleccionado")
+        ], fontsize=10)
+
+        # Mostrar la gráfica
+        plt.tight_layout()
+        plt.show()
