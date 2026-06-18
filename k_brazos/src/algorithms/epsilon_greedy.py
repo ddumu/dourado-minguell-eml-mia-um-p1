3571c4138a2e162439609cd208a2af655ff3f3fe
ddumu <denis.dourado@um.es>
2026-06-18 16:21:41 +0200

    Fix import path for Algorithm in epsilon_greedy.py

--- k_brazos/src/algorithms/epsilon_greedy.py
+++ k_brazos/src/algorithms/epsilon_greedy.py
@@ -15,7 +15,7 @@ For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
 import numpy as np
 
 
-from algorithms.algorithm import Algorithm
+from src.algorithms.algorithm import Algorithm
 
 class EpsilonGreedy(Algorithm):
 
